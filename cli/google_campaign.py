997f82bc639317794e9732e78fb47f4218e55afe
Simonbelete <simonbelete@gmail.com>
2021-04-23 14:06:58 +0300

    feat(cli): update google campign cli

---
+++ cli/google_campaign.py
@@ -0,0 +1,66 @@
+import argparse
+
+import os
+import argparse
+import datetime
+
+from google.ads.googleads.errors import GoogleAdsException
+from google.ads.googleads.client import GoogleAdsClient
+from google.api_core import protobuf_helpers
+
+from configuration import CONFIG
+
+cwd = os.getcwd() # Current directory 
+_DATE_FORMAT = CONFIG.get('DEFAULT', 'DATE_FORMAT')
+googleads_client = GoogleAdsClient.load_from_storage(f'{cwd}/google-ads.yaml')
+
+def update_google_campaign(customer_id, campaign_id):
+    global googleads_client, _DATE_FORMAT
+    client = googleads_client
+    campaign_service = client.get_service("CampaignService")
+    # Create campaign operation.
+    campaign_operation = client.get_type("CampaignOperation")
+    campaign = campaign_operation.update
+
+    campaign_status = campaign_status_enum = client.get_type(
+                            "CampaignStatusEnum"
+                        ).CampaignStatus.PAUSED
+
+    campaign.status = campaign_status
+
+    campaign.resource_name = campaign_service.campaign_path(
+        customer_id, campaign_id
+    )
+    
+    campaign.network_settings.target_search_network = False
+    # Retrieve a FieldMask for the fields configured in the campaign.
+    client.copy_from(
+        campaign_operation.update_mask,
+        protobuf_helpers.field_mask(None, campaign._pb),
+    )
+
+    campaign_response = campaign_service.mutate_campaigns(
+        customer_id=customer_id, operations=[campaign_operation]
+    )
+
+    print(f"Updated campaign {campaign_response.results[0].resource_name}.")
+
+
+parser = argparse.ArgumentParser(
+    description="Updates the given campaign for the specified customer."
+    )
+# The following argument(s) should be provided to run the example.
+parser.add_argument(
+    "-c",
+    "--customer_id",
+    type=str,
+    required=True,
+    help="The Google Ads customer ID.",
+)
+parser.add_argument(
+    "-i", "--campaign_id", type=str, required=True, help="The campaign ID."
+)
+args = parser.parse_args()
+
+
+update_google_campaign(args.customer_id, args.campaign_id)
--- main.py
+++ main.py
@@ -1,8 +1,15 @@
+import os
 import db
 
+from crontab import CronTab
+from google_campaign import update_google_campaign
+from configuration import CONFIG
 from onair import OnAir
 
 if __name__ == '__main__':
+    cwd = os.getcwd() # Current directory 
+    cron = CronTab(user=True)
+
     connection = db.connect()
     # onair table
     onair = OnAir(connection)
@@ -10,4 +17,15 @@ if __name__ == '__main__':
     campaigns = onair.get_campains()
     
     for campaign in campaigns:
-        # Start campign
+        # Since google allow ads to run a minimum of 1 day, set the end date to
+        # a minimum of 1 day else it wont work 
+        end_second = campaign[3]
+        if int(campaign[3]) < 86400: # 86400 = 1 day
+            end_second = 86400
+
+        # Start running the campign
+        update_google_campaign(customer_id=campaign[0], campaign_id=campaign[1], campaign_time=end_second, status=CONFIG.get('DEFAULT', 'START_CAMPAIGN'))
+
+        # Set a cron job
+        cmd = f"python{CONFIG.get('DEFAULT', 'PYTHON_VERSION')} {cwd}/"
+        job = cron.new(command=cmd)
